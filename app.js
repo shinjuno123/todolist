9cf74e409961db85f61150b425e345c552d7b879
shinjuno123 <shinjuno123@naver.com>
2022-09-03 22:43:07 +0900

    feat: Connect MongoDB by Mongoose and initialize

--- app.js
+++ app.js
@@ -2,16 +2,50 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 
 const app = express();
 const port = 8080;
-const items = [];
-const workItems = [];
+
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemsSchema =  new mongoose.Schema({
+  name : {
+    type : String,
+    required : true
+  }
+});
+
+const Item = mongoose.model("items",itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome!"
+})
+const item2 = new Item({
+  name: "Hit the + button to add a new item"
+})
+
+const item3 = new Item({
+  name: "<--- Hit this to delete an item"
+})
+
+const defaultItems = [item1,item2,item3]
+
+Item.insertMany(defaultItems, function(err){
+  if (err){
+    console.log(err);
+  }else{
+    console.log("Succeeded to insert 3 default items");
+  }
+});
+
 
 app.get("/", function (req, res) {
   const day = date.getDate();
